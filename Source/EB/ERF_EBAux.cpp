8fea7f08dd48bb55ee7b89efa0aa4a5dcfbe461d
Soonpil Kang <spkang1123@gmail.com>
2026-01-31 14:28:29 -0800

    Remove the condition for domain boundary in eb_aux_.

--- Source/EB/ERF_EBAux.cpp
+++ Source/EB/ERF_EBAux.cpp
@@ -848,12 +848,6 @@ define( [[maybe_unused]] int const& a_level,
     const Box& bx = mfi.validbox();
     const Box& bx_grown = mfi.growntilebox();
     const Box domain = surroundingNodes(a_geom.Domain(), a_idim);
-    const int dom_lo_i = domain.smallEnd(0);
-    const int dom_hi_i = domain.bigEnd(0) + 1;
-    const int dom_lo_j = domain.smallEnd(1);
-    const int dom_hi_j = domain.bigEnd(1) + 1;
-    const int dom_lo_k = domain.smallEnd(2);
-    const int dom_hi_k = domain.bigEnd(2) + 1;
 
     Array4<EBCellFlag> const& aux_flag  = m_cellflags->array(mfi);
     Array4<Real>       const& aux_vfrac = m_volfrac->array(mfi);
@@ -877,15 +871,11 @@ define( [[maybe_unused]] int const& a_level,
       int xbx_hi = my_xbx.bigEnd(0);
       ParallelFor(my_xbx, [=] AMREX_GPU_DEVICE (int i, int j, int k) noexcept
       {
-        if (aux_vfrac(i,j,k) < small_volfrac || aux_vfrac(i-1,j,k) < small_volfrac) {
-          if ((i == dom_lo_i && aux_vfrac(i,j,k) < small_volfrac) ||
-              (i == dom_hi_i && aux_vfrac(i-1,j,k) < small_volfrac) ||
-              (i != dom_lo_i && i != dom_hi_i) ||
-              (i == xbx_lo && aux_vfrac(i,j,k) < small_volfrac) ||
-              (i == xbx_hi && aux_vfrac(i-1,j,k) < small_volfrac) ||
-              (i != xbx_lo && i != xbx_hi)) {
-              aux_afrac_x(i,j,k) = 0.0;
-          }
+        if ((i == xbx_lo && aux_vfrac(i,j,k) < small_volfrac) ||
+            (i == xbx_hi && aux_vfrac(i-1,j,k) < small_volfrac) ||
+            (i > xbx_lo && i < xbx_hi &&
+            (aux_vfrac(i,j,k) < small_volfrac || aux_vfrac(i-1,j,k) < small_volfrac))) {
+            aux_afrac_x(i,j,k) = 0.0;
         }
       });
 
@@ -894,15 +884,11 @@ define( [[maybe_unused]] int const& a_level,
       int ybx_hi = my_ybx.bigEnd(1);
       ParallelFor(my_ybx, [=] AMREX_GPU_DEVICE (int i, int j, int k) noexcept
       {
-        if (aux_vfrac(i,j,k) < small_volfrac || aux_vfrac(i,j-1,k) < small_volfrac) {
-          if ((j == dom_lo_j && aux_vfrac(i,j,k) < small_volfrac) ||
-              (j == dom_hi_j && aux_vfrac(i,j-1,k) < small_volfrac) ||
-              (j != dom_lo_j && j != dom_hi_j) ||
-              (j == ybx_lo && aux_vfrac(i,j,k) < small_volfrac) ||
-              (j == ybx_hi && aux_vfrac(i,j-1,k) < small_volfrac) ||
-              (j != ybx_lo && j != ybx_hi)) {
-                aux_afrac_y(i,j,k) = 0.0;
-          }
+        if ((j == ybx_lo && aux_vfrac(i,j,k) < small_volfrac) ||
+            (j == ybx_hi && aux_vfrac(i,j-1,k) < small_volfrac) ||
+            (j > ybx_lo && j < ybx_hi &&
+            (aux_vfrac(i,j,k) < small_volfrac || aux_vfrac(i,j-1,k) < small_volfrac))) {
+            aux_afrac_y(i,j,k) = 0.0;
         }
       });
 
@@ -911,15 +897,11 @@ define( [[maybe_unused]] int const& a_level,
       int zbx_hi = my_zbx.bigEnd(2);
       ParallelFor(my_zbx, [=] AMREX_GPU_DEVICE (int i, int j, int k) noexcept
       {
-        if (aux_vfrac(i,j,k) < small_volfrac || aux_vfrac(i,j,k-1) < small_volfrac) {
-          if ((k == dom_lo_k && aux_vfrac(i,j,k) < small_volfrac) ||
-              (k == dom_hi_k && aux_vfrac(i,j,k-1) < small_volfrac) ||
-              (k != dom_lo_k && k != dom_hi_k) ||
-              (k == zbx_lo && aux_vfrac(i,j,k) < small_volfrac) ||
-              (k == zbx_hi && aux_vfrac(i,j,k-1) < small_volfrac) ||
-              (k != zbx_lo && k != zbx_hi)) {
-              aux_afrac_z(i,j,k) = 0.0;
-          }
+        if ((k == zbx_lo && aux_vfrac(i,j,k) < small_volfrac) ||
+            (k == zbx_hi && aux_vfrac(i,j,k-1) < small_volfrac) ||
+            (k > zbx_lo && k < zbx_hi &&
+            (aux_vfrac(i,j,k) < small_volfrac || aux_vfrac(i,j,k-1) < small_volfrac))) {
+            aux_afrac_z(i,j,k) = 0.0;
         }
       });
 
